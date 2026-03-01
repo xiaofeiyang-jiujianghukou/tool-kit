eecdfda4f7c4adea4c1af5e0a49e53517738fead
许飞杨19930112 <2522894142@qq.com>
2026-03-02 02:01:58 +0800

    测试

---
+++ src/main/java/com/vision/tool/kit/test/TestAI.java
@@ -0,0 +1,10 @@
+package com.vision.tool.kit.test;
+
+public class TestAI {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = a/ b;
+        System.out.println(c);
+    }
+}
