0dc73b31bed5aa9154e3e0fb6af6dac159363dfb
许飞杨19930112 <2522894142@qq.com>
2026-03-02 09:18:03 +0800

    feat: add new function with potential bugs

--- src/main/java/com/vision/tool/kit/test/TestAI.java
+++ src/main/java/com/vision/tool/kit/test/TestAI.java
@@ -3,11 +3,12 @@ package com.vision.tool.kit.test;
 public class TestAI {
     public static void main(String[] args) {
         int a = 10;
-        int b = 0;
-        int c = a/ b;
-        Integer d = null;
+        int b = 1;
+        int c = a / b;
+        Integer d = 2;
         int e = a / d;
         System.out.println( 10 / d);
         System.out.println(c);
+        System.out.println(e);
     }
 }
